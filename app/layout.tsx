622e915ba04cf0d5cbb79fabdfac036dd91bc089
unknown <khaymanii>
2026-03-20 08:11:03 +0100

    add privacy policy and terms of service pages; update category data structure with IDs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,17 @@
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import { AuthInit } from "@/Components/providers/AuthInit";
+import { Metadata } from "next";
 
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   display: "swap",
 });
+const metadata: Metadata = {
+  title: "Saint - Relentless",
+  description: "Sportswear and Gears for the Relentless",
+};
 
 export default function RootLayout({
   children,

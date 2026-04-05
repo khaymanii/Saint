5f5bff28c84c4f5e8765218244835330de2df786
unknown <khaymanii>
2026-04-05 09:01:58 +0100

    refactor: update imports and use serverTimestamp for user creation

--- store/useAuthStore.tsx
+++ store/useAuthStore.tsx
@@ -7,10 +7,10 @@ import {
   signOut,
   onAuthStateChanged,
   User,
+  signInWithRedirect,
 } from "firebase/auth";
-import { doc, setDoc, getDoc } from "firebase/firestore";
-import { auth } from "@/firebaseConfig/firebase";
-import { db } from "@/firebaseConfig/firebase";
+import { doc, setDoc, getDoc, serverTimestamp } from "firebase/firestore";
+import { auth, db } from "@/firebaseConfig/firebase";
 import { useCartStore } from "@/store/useCartStore";
 import { getIdToken } from "firebase/auth";
 
@@ -47,7 +47,7 @@ export const useAuthStore = create<AuthState>((set) => ({
           name: user.displayName,
           email: user.email,
           photoURL: user.photoURL,
-          createdAt: new Date(),
+          serverTimestamp,
         });
       }
 
